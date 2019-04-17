e5a199305154e97964c8bfab5424a8d91a22c8df
Edson Servin <edson@ciencias.unam.mx>
2019-04-17 13:24:08 -0500

    #3 Se calcula el lado mínimo

--- Year-2015/dia02/dia_02_1.groovy
+++ Year-2015/dia02/dia_02_1.groovy
@@ -3,6 +3,8 @@ def wrappingPaper(l, w, h){
 	side_one = w*h//l
 	side_two = l*h//w
 	side_three = l*w//h
+	def smallest = [side_one, side_two, side_three] 
+	println smallest.min()
 
 	2*side_one+2*side_two+2*side_three
 }
