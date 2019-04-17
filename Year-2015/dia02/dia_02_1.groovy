04c3d9eb9695e594a43435bdf27c6c51277ee042
Edson Servin <edson@ciencias.unam.mx>
2019-04-17 13:13:38 -0500

    #3 Se creó el método principal del problema

--- Year-2015/dia02/dia_02_1.groovy
+++ Year-2015/dia02/dia_02_1.groovy
@@ -1,5 +1,15 @@
 def f = new File("input")
-f.eachLine() {
+def wrappingPaper(l, w, h){
+	side_one = w*h//l
+	side_two = l*h//w
+	side_three = l*w//h
+
+	2*side_one+2*side_two+2*side_three
+}
+
+println wrappingPaper(2,3,4)
+
+/*f.eachLine() {
 	line -> 
 	println "$line"
-}
+}*/
