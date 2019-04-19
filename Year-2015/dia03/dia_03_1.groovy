443ad97e7c96b57ebe915383d9c8d163dfb50f5f
Edson Servin <edson@ciencias.unam.mx>
2019-04-19 11:45:52 -0500

    #5 Primera parte terminada

--- Year-2015/dia03/dia_03_1.groovy
+++ Year-2015/dia03/dia_03_1.groovy
@@ -3,10 +3,34 @@ def houses =[:]
 def x = 0
 def y = 0
 def position = new Tuple2(x,y)
-println position
 houses[position] = 1
 f.eachLine() {
 	line -> 
 	for(char c: line){
+		def xi = x
+		def yi = y
+		if (c == '^'){ 
+			y += 1 
+			position = new Tuple2(x,y)
+		}else if (c == 'v'){
+			y -= 1
+			position = new Tuple2(x,y)
+		}else if (c == '<'){
+			x -= 1
+			position = new Tuple2(x,y)
+		}else if (c == '>'){
+			x += 1
+			position = new Tuple2(x,y)
+		}
+		if (houses.containsKey(position)){
+			def val = houses[position]
+			houses[position] = val +1;
+		}else houses[position] = 1
 	}
 }
+total = 0
+for (Tuple2 key: houses.keySet()){
+	if (houses[key] >= 1)
+	total += 1
+}
+println total
