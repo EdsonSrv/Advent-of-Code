3ef530ddbe33543f74b20c775c01f1bbdcf6e09c
Edson Servin <edson@ciencias.unam.mx>
2019-04-19 11:00:59 -0500

    #5 Probando tuplas

--- Year-2015/dia03/dia_03_1.groovy
+++ Year-2015/dia03/dia_03_1.groovy
@@ -1,4 +1,10 @@
 def f = new File("input")
+def houses =[:]
+def x = 0
+def y = 0
+def position = new Tuple2(x,y)
+println position
+houses[position] = 1
 f.eachLine() {
 	line -> 
 	for(char c: line){
