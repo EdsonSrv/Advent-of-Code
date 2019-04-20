8508da1e20e306b45d363777334a3e1ca9d93fc4
Edson Servin <edson@ciencias.unam.mx>
2019-04-20 12:50:49 -0500

    #9 Se prepara el método principal y las variables a evaluar

---
+++ Year-2015/dia05/dia_05_1.groovy
@@ -0,0 +1,20 @@
+/*
+ *
+ * Dia 5 
+ * String's nice:
+ * 1.- Contiene al menos 3 vocales
+ * 2.- Contiene al menos una letra seguida dos veces
+ * 3.- No contiene las subcadenas; ab, cd, pq, xy.
+ *
+ */
+
+ def example1 = "ugknbfddgicrmopn" //is nice
+ def example2 = "aaa" //is nice
+ def example3 = "jchzalrnumimnmhp" //is naughty
+ def example4 = "haegwjzuvuyypxyu" // is naughty
+ def example5 = "dvszwmarrgswjxmb" // is naughty
+
+ def isNice(String string){
+ 	def is_nice
+ 	
+ }
