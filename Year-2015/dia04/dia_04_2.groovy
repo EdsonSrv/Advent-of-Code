f90b68ca9354b8d29f4be7df1507544d76901003
Edson Servin <edson@ciencias.unam.mx>
2019-04-19 20:36:27 -0500

    #7 parte dos terminada

---
+++ Year-2015/dia04/dia_04_2.groovy
@@ -0,0 +1,28 @@
+import java.security.MessageDigest
+
+def md5Hash(String input){
+	def md = MessageDigest.getInstance("MD5")
+	md.update(input.getBytes())
+	byte[] digest = md.digest()
+	def sb = new StringBuffer()
+	for (byte b : digest) {
+		sb.append(String.format("%02x", b & 0xff))
+	}
+	sb.toString()
+}
+
+def anwer(String secretKey) {
+	def i = 0
+	while (true) {
+		def input = secretKey + i
+		if (md5Hash(input)
+					.substring(0, 6)
+					.equals("000000")){
+			break
+		}
+		i++
+	}
+	i 
+}
+
+println anwer("iwrupvqb")
