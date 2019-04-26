03d1cfb2c37252f8f9af0c6a435755d03c05dcd6
Edson Servin <edson@ciencias.unam.mx>
2019-04-26 13:13:04 -0500

    #1 Se modificó para una sola línea del input

--- Year-2015/dia01/dia_01.groovy
+++ Year-2015/dia01/dia_01.groovy
@@ -1,10 +1,8 @@
-def f = new File("input")
+def line
+new File("input").withReader { line = it.readLine() }  
 def floor = 0
-f.eachLine() {
-	line -> 
-	for(char c: line){
-		if (c == '(') floor += 1
-		if (c == ')') floor -= 1
-	}
+for(char c: line){
+	if (c == '(') floor += 1
+	if (c == ')') floor -= 1
 }
 println "$floor"
