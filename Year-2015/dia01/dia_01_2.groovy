34698cba83c2cfaba526a4753661da04a7780a62
Edson Servin <edson@ciencias.unam.mx>
2019-04-26 13:18:05 -0500

    #1 resolved Sólo hay una línea en el archivo

--- Year-2015/dia01/dia_01_2.groovy
+++ Year-2015/dia01/dia_01_2.groovy
@@ -1,13 +1,11 @@
-def f = new File("input")
 def floor = 0
 def position = 1
-f.eachLine() {
-	line -> 
-	for(char c: line){
-		if (c == '(') floor += 1
-		if (c == ')') floor -= 1
-		if (floor == -1) break
-		position += 1
-	}
+def line
+new File("input").withReader { line = it.readLine() }
+for(char c: line){
+  if (c == '(') floor += 1
+  if (c == ')') floor -= 1
+  if (floor == -1) break
+  position += 1
 }
 println "$position"
